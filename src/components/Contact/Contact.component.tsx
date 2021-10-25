61d43536d27a71ceb0be181c51432e8f4fed4c3d
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-10-25 16:26:17 +0300

    Created responsive component the displays the minimal contact info

--- src/components/Contact/Contact.component.tsx
+++ src/components/Contact/Contact.component.tsx
@@ -1,6 +1,15 @@
 const Contact: React.FC<ContactProps> = ({ contact }) => {
-  console.log(contact);
-  return <div></div>;
+  return (
+    <div className="flex flex-col border p-2">
+      <div className="flex mb-1">
+        <span className="font-bold mr-2">{contact.fullname}</span>
+        <span>{contact.phone}</span>
+      </div>
+      <p className="text-gray-500 text-sm">
+        {contact.description || "No description"}
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
